b9eb30f25a854a0ceab102fe4289589c61e954c2
joshuakiua <joshuamwendwa91@gmail.com>
2024-01-10 20:12:05 +0300

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 
 def square_matrix(matrix=[]):
     squares = []
-    for mat in matrix:
-        squares.append([n**2 for n in mat])
-    return squares
+    for line in matrix:
+        squared.append([c**2 for c in line])
+    return squared
