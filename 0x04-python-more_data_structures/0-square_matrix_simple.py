44782738d7bc67f190d3923219116e523b3bfe0e
joshuakiua <joshuamwendwa91@gmail.com>
2024-01-10 21:26:59 +0300

    Squared

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-def square_matrix(matrix=[]):
-    squares = []
+def square_matrix(matrix = []):
+    squared = []
     for line in matrix:
         squared.append([c**2 for c in line])
     return squared
